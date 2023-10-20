84888ab143eeaf9f17422c82c8276cc0983ade01
Sergio de Morais <srdemorais.sandbox@gmail.com>
2023-10-20 14:05:50 +0200

    add user tests for next / previous musical notes

--- musicalnotes/musicalnotes.go
+++ musicalnotes/musicalnotes.go
@@ -1,10 +1,17 @@
 package musicalnotes
 
+import "fmt"
+
 var notes = [7]string{"Do", "Re", "Mi", "Fa", "So", "La", "Si"}
 
 type MusicalNote struct {
 	Note  string
 	Sound string
+}
+
+func (n *MusicalNote) TestUser() {
+	(*n).CheckNext()
+	(*n).CheckPrevious()
 }
 
 func (n *MusicalNote) GetNext() string {
@@ -35,4 +42,24 @@ func (n *MusicalNote) GetPrevious() string {
 		}
 	}
 	return next
+}
+
+func (n *MusicalNote) CheckNext() {
+	next := GetNext(n)
+	fmt.Printf("What is the note after \"%v\" ? ", next)
+
+	var iNext string
+	fmt.Scanln(&iNext)
+
+	fmt.Println("Result: ", next == iNext)
+}
+
+func (n *MusicalNote) CheckPrevious() {
+	previous := GetPrevious(n)
+	fmt.Printf("What is the note before \"%v\" ? ", previous)
+
+	var iPrevious string
+	fmt.Scanln(&iPrevious)
+
+	fmt.Println("Result: ", previous == iPrevious)
 }
